263bda43dccf611bc1f5503521adbfafcf72dbe9
Mulong <dsh15325@163.com>
2019-11-27 13:50:02 +1100

    add usable ajax upload

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 var express = require('express');
 var formidable = require("formidable");
 var fs = require('fs');
+var upload = require('multer')({dest:'uploads/'});
 
 var app = express();
 
 app.use('/public', express.static('public'));
+app.use('/uploads', express.static('uploads'));
 
 app.get('/', function(req, res){
     res.sendfile('public/index.html')
 });
 
-app.post('/upload', function (req, res) {
+app.post('/upload', upload.single('uploadImg'), function (req, res) {
     var form = new formidable.IncomingForm();
     form.parse(req, function(error, fields, files) {
         console.log(files.upload.path);
