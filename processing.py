f0ad1ba5211c73bd6907d5a6586bddd6b7cbc897
Mulong <dsh15325@163.com>
2019-11-28 16:34:20 +1100

    start adding processing

--- processing.py
+++ processing.py
@@ -1,10 +1,12 @@
 import cv2
 import sys
+import time
 
+start = time.clock()
 index = sys.argv[1]
 
 img = cv2.imread('uploads/' + str(index) + '.png')
 edges = cv2.Canny(img,100,200)
 
 cv2.imwrite('processed/pro' + str(index) + '.png', edges)
-print("Processing complete")
+print("Processing complete, time taken: %.3fs" % (time.clock() - start))
